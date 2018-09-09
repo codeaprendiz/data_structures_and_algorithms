ab8c64ecebff890135c33cf9a927fd190f0c5069
Ankit <codeaprendiz@gmail.com>
2018-09-09 18:32:06 +0530

    Update SinglyLinkedList.java

--- dataStructuresAndAlgorithms/linkedList/SinglyLinkedList.java
+++ dataStructuresAndAlgorithms/linkedList/SinglyLinkedList.java
@@ -65,6 +65,19 @@ class SinglyLinkedList {
 		if(cur==null) return;
 		prev.next=cur.next;
 	}
+	
+	boolean hasCycle() {
+     		Node slow_p = head, fast_p = head; 
+        	while (slow_p != null && fast_p != null && fast_p.next != null) { 
+            		slow_p = slow_p.next; 
+            		fast_p = fast_p.next.next; 
+            		if (slow_p == fast_p) { 
+                		//System.out.println("Found loop"); 
+                		return true; 
+            		} 
+        	} 
+        	return false; 
+	}
 
 	public static void main(String args[]) {
 		SinglyLinkedList SLL=new SinglyLinkedList();
