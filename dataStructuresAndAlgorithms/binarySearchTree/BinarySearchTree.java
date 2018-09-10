41bc15640fa719984428336c9d20ca3c9be44d26
Ankit <codeaprendiz@gmail.com>
2018-09-10 09:52:59 +0530

    Update BinarySearchTree.java

--- dataStructuresAndAlgorithms/binarySearchTree/BinarySearchTree.java
+++ dataStructuresAndAlgorithms/binarySearchTree/BinarySearchTree.java
@@ -79,6 +79,27 @@ class BinarySearchTree {
 		}
 		return prev.val;
 	}
+	
+/************************************************************************ 
+Function to find Lowest Common Ancestor(LCA) of n1 and n2. The function 
+assumes that both n1 and n2 are present in BST 
+ *************************************************************************/
+    	Node lca(Node node, int n1, int n2) 
+    	{
+        	if (node == null)
+           		return null;
+  
+        	// If both n1 and n2 are smaller than root, then LCA lies in left
+        	if (node.val > n1 && node.val > n2)
+            		return lca(node.left, n1, n2);
+  
+       		 // If both n1 and n2 are greater than root, then LCA lies in right
+        	if (node.val < n1 && node.val < n2) 
+          	 	return lca(node.right, n1, n2);
+  
+        	return node;
+   	 }
+
 
 	public static void main(String[] args) {
 		BinarySearchTree bt=new BinarySearchTree(new Node(20));
